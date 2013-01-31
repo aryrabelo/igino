05e2e1c7017771dca260f9d2f8cebe986b8729e4
Ary Rabelo <aryrabelo@gmail.com>
2013-01-31 16:53:39 -0300

    first commit

---
+++ lvl1/index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  
+  <link href="style.css" rel="stylesheet" type="text/css">
+  
+</head>
+<body>
+<?
+  @include(menu.php);
+?>
+
+
+  
+  olá Mundo.  
+  <a href="mundo.html">Ver mundo</a>
+  
+  
+
+  
+  
+  
+  
+  
+  
+</body>
+
+
+</html>
---
+++ lvl1/menu.php
@@ -0,0 +1,6 @@
+
+  <div id="menu">
+    <a href="index.html">Pagina Inicial</a>
+    <a href="mundo.html">Mundo</a>
+    <a href="brasil.html">Brasil</a>
+  </div>
